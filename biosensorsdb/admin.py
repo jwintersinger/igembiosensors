c44a51fff4b28c721909b8e5f4a56235dcdf2276
Jeff Wintersinger <jeff.git@wintersinger.com>
2013-08-28 01:21:32 -0600

    Add admin search field.

--- biosensorsdb/admin.py
+++ biosensorsdb/admin.py
@@ -1,10 +1,13 @@
 import biosensorsdb.models
 from django.contrib import admin
 
+class ProjectAdmin(admin.ModelAdmin):
+  search_fields = ['team__name', 'year', 'title', 'abstract', 'inputs__name', 'outputs__name', 'tags__name']
+
 admin.site.register(biosensorsdb.models.Team)
 admin.site.register(biosensorsdb.models.SensorInput)
 admin.site.register(biosensorsdb.models.SensorOutput)
 admin.site.register(biosensorsdb.models.Track)
 admin.site.register(biosensorsdb.models.Application)
 admin.site.register(biosensorsdb.models.CompetitionResult)
-admin.site.register(biosensorsdb.models.Project)
+admin.site.register(biosensorsdb.models.Project, ProjectAdmin)
