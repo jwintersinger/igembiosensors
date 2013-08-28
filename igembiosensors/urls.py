7056c2609a792fa2fa71734f7b91739c5076ae27
Jeff Wintersinger <jeff.git@wintersinger.com>
2013-08-28 01:53:34 -0600

    Redirect index to admin.

--- igembiosensors/urls.py
+++ igembiosensors/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
+from django.http import HttpResponseRedirect
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'igembiosensors.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', lambda request: HttpResponseRedirect('/admin/')),
     url(r'^admin/', include(admin.site.urls)),
 )
