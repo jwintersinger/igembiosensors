0fb6050be903d8e27af61a68d071e467c10d4d6a
Jeff Wintersinger <jeff.git@wintersinger.com>
2013-09-02 19:38:01 -0600

    Truncate abstracts.

--- biosensorsdb/static/js/main.js
+++ biosensorsdb/static/js/main.js
@@ -1,5 +1,38 @@
+function truncate(elem, length) {
+  elem = $(elem);
+  elem.text(elem.text().substring(length) + '...');
+  elem.data('truncated', true);
+}
+
+function untruncate(elem, full_text_key) {
+  elem = $(elem);
+  elem.text(elem.data(full_text_key));
+  elem.data('truncated', false);
+}
+
 $(function() {
+  // Configure table sorting.
   $('#projects').stupidtable();
   $('#projects th').append('<span class="glyphicon asc-icon glyphicon-circle-arrow-down"></span>' +
     '<span class="glyphicon desc-icon glyphicon-circle-arrow-up"></span>');
+
+  // Truncate abstracts.
+  var truncate_length = 76;
+  $('#projects .abstract').each(function() {
+    var abs = $(this);
+    var full_abstract = abs.text();
+
+    if(full_abstract.length <= truncate_length)
+      return;
+
+    abs.data('full_abstract', full_abstract);
+    truncate(abs, truncate_length);
+  }).click(function() {
+    var abs = $(this);
+    if(abs.data('truncated')) {
+      untruncate(abs, 'full_abstract');
+    } else {
+      truncate(abs, truncate_length);
+    }
+  });
 });
