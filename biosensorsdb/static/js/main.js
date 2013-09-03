902cd607534bfb6b03925bf2427f5cc3c497bc0d
Jeff Wintersinger <jeff.git@wintersinger.com>
2013-09-02 18:01:31 -0600

    Display icon in sorted column headers.

--- biosensorsdb/static/js/main.js
+++ biosensorsdb/static/js/main.js
@@ -1,3 +1,5 @@
 $(function() {
   $('#projects').stupidtable();
+  $('#projects th').append('<span class="glyphicon asc-icon glyphicon-circle-arrow-down"></span>' +
+    '<span class="glyphicon desc-icon glyphicon-circle-arrow-up"></span>');
 });
